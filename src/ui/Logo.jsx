c56738a6774d8730e0d44a47920b60d876df4895
ramombarbalho <barbalhoramom@gmail.com>
2024-01-18 05:56:52 -0300

    fix logo

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { useDarkMode } from "../context/DarkModeContext";
 
 const StyledLogo = styled.div`
   text-align: center;
@@ -12,17 +11,9 @@ const StyledLogo = styled.div`
 `;
 
 function Logo() {
-  const { isDarkMode } = useDarkMode();
-
-  const logoFilter = isDarkMode ? "" : "invert(100%)";
-
   return (
     <StyledLogo>
-      <img
-        style={{ filter: logoFilter }}
-        src="./logo-sigfap.png"
-        alt="softex logo"
-      />
+      <img src="./logo-sigfap.png" alt="softex logo" />
     </StyledLogo>
   );
 }
